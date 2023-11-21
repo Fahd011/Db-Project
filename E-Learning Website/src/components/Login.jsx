d9c40709f7fde65b9b7fc95938f1d1b39758664c
Fahd Aleem <fahdaleem256@gmail.com>
2023-11-21 18:22:01 +0500

    Updated login page

--- E-Learning Website/src/components/Login.jsx
+++ E-Learning Website/src/components/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import '../styleSheets/Login.css'
+import { Link } from 'react-router-dom';
 
 export default function Login(){
     return(
-        <div>
+        <div className="login-container">
+            <img src="../images/loginImage1.png" alt="" />
             <form action="Login" className="loginForm">
+                <img className="Logo" src="../images/1.png" alt="" />
                 <h1 className="heading">Login</h1>
                 <input type="email" placeholder="Email" className="textbox"/>
                 <input type="password" placeholder="Password" className="textbox"/>
@@ -12,11 +15,13 @@ export default function Login(){
                     <div className="RememberMe">
                         <input type="checkbox" /><label>Remember me</label>
                     </div>
-                    <a href="" text-align="right">Forgot Password?</a>
+                    <Link to="/reset-pass">Forgot Password</Link>
                 </div>
-                <button>Login</button>
+                <Link to="/home">
+                <button type="submit">Login</button>
+                </Link>
                 <div className="footer">
-                    <h3>Don't have an account? <a href="">Register</a></h3>
+                    <h3>Don't have an account? <Link to="/register">Register</Link></h3>
                 </div>
             </form>
         </div>
